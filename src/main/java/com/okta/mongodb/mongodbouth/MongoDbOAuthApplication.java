dd75cf3172a14f8de0f5d735b18fd9bd684e7be0
Siwar Mansouri <mansourisiwar@gmail.com>
2019-12-27 15:03:25 +0100

    add project

---
+++ src/main/java/com/okta/mongodb/mongodbouth/MongoDbOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.okta.mongodb.mongodbouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okta/mongodb/mongodbouth/MongoDbOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.mongodb.mongodbouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
